5512477575f43d703fafb890c85b370713619846
Arslan <nikolaevarslan1@yandex.ru>
2024-04-24 22:34:31 +0300

    done a lot of design features

---
+++ app/src/main/java/ru/kpfu/itis/nikolaev/delivery/ui/fragments/TestFragment.kt
@@ -0,0 +1,25 @@
+package ru.kpfu.itis.nikolaev.delivery.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.kpfu.itis.nikolaev.delivery.R
+
+class TestFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test, container, false)
+    }
+
+}
