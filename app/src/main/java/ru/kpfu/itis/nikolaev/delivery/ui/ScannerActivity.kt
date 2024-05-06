990a794d0439832585e41b2039ab8862ee5f264c
Arslan Nikolaev <nikolaevarslan1@yandex.ru>
2024-05-06 11:18:33 +0300

    change 2nd ScannerActivity to fragment

--- app/src/main/java/ru/kpfu/itis/nikolaev/delivery/ui/ScannerActivity.kt
+++ app/src/main/java/ru/kpfu/itis/nikolaev/delivery/ui/ScannerActivity.kt
@@ -3,15 +3,23 @@ package ru.kpfu.itis.nikolaev.delivery.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import me.dm7.barcodescanner.zbar.Result
 import me.dm7.barcodescanner.zbar.ZBarScannerView
+import ru.kpfu.itis.nikolaev.delivery.R
 
-class ScannerActivity : AppCompatActivity(), ZBarScannerView.ResultHandler {
+class ScannerActivity : Fragment(), ZBarScannerView.ResultHandler {
     private lateinit var zbView: ZBarScannerView
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         super.onCreate(savedInstanceState)
-        zbView = ZBarScannerView(this)
-        setContentView(zbView)
+        zbView = ZBarScannerView(requireContext())
+        return zbView.rootView
     }
 
     override fun onResume() {
@@ -27,6 +35,5 @@ class ScannerActivity : AppCompatActivity(), ZBarScannerView.ResultHandler {
 
     override fun handleResult(result: Result) {
         Log.d("MyLog",result.contents.toString())
-        finish()
     }
 }
