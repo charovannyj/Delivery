4cc2e8aa1db4168d76a25720f6688a283bf9d4f5
Arslan Nikolaev <nikolaevarslan1@yandex.ru>
2024-05-20 12:07:47 +0300

    add qr photo on item_order

--- app/src/main/java/ru/kpfu/itis/nikolaev/delivery/presentation/adapters/CustomAdapter.kt
+++ app/src/main/java/ru/kpfu/itis/nikolaev/delivery/presentation/adapters/CustomAdapter.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
@@ -25,6 +26,7 @@ class CustomAdapter(
         val tv_status: TextView = view.findViewById(R.id.tv_status)
         val tv_date: TextView = view.findViewById(R.id.tv_date)
         val tv_info: TextView = view.findViewById(R.id.tv_info)
+        val iv_qr: ImageView = view.findViewById(R.id.iv_qr)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
@@ -33,13 +35,14 @@ class CustomAdapter(
         return ViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val order = orders[position]
         viewHolder.tv_status.text = order.status// Используем данные из OrderModel
         //viewHolder.tv_status.text = "в ожидании принятия"
         viewHolder.tv_date.text = convertFullDateToSimple(order.date)
         viewHolder.tv_info.text = order.addressTo
-        viewHolder.itemView.setOnClickListener {  // Обрабатываем клик на элементе
+        viewHolder.iv_qr.setOnClickListener {  // Обрабатываем клик на элементе
             onClickListener.invoke(order)  // Вызываем onClickListener, передавая OrderModel
         }
     }
